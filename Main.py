44a70770ce3d5191730593ee18ab5768ae20d7cb
Nelson <nelsonfcmilheiro@gmail.com>
2023-01-05 00:02:00 +0000

    First commit

---
+++ Main.py
@@ -0,0 +1,33 @@
+import pygame
+
+def main():
+
+    pygame.init()
+
+    res = (640, 360)
+    screen = pygame.display.set_mode(res)
+
+    exit = False
+    while(not exit):
+
+        events = pygame.event.get()
+        for evt in events:
+            if (evt.type == pygame.QUIT):
+                exit = True
+
+        screen.fill((0, 0, 20))
+
+        mouse_button = pygame.mouse.get_pressed()
+        cursor_color = (0, 200, 200)
+
+        if (mouse_button[0]):
+            cursor_color = (200, 200, 0)
+        elif (mouse_button[2]):
+            cursor_color = (0, 200, 0)
+
+        mouse_pos = pygame.mouse.get_pos()
+        pygame.draw.circle(screen, cursor_color, mouse_pos, 5, 5)
+
+        pygame.display.flip()
+
+main()
